5febd6d24254e6894a4837f03008009ff0794865
Jay Sullivan <JaySull514@gmail.com>
2018-03-26 21:41:07 -0500

    About grid tweak

--- Components/Grids/AboutGrid.js
+++ Components/Grids/AboutGrid.js
@@ -4,7 +4,7 @@ export default styled('div')`
   width: 100%;
   grid-column-start: 2;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: minmax(400px, 1fr) minmax(400px, 1fr);
   grid-template-rows: auto;
   grid-gap: 20px;
 `;
