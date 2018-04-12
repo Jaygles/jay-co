4b3909aaed11ac1087ecf1575ce696a137d481df
Jay Sullivan <JaySull514@gmail.com>
2018-04-12 18:34:06 -0500

    postgres config was strings

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize');
 
 module.exports = {
   development: {
-    username: 'process.env.RDS_USERNAME',
-    password: 'process.env.RDS_PASSWORD',
-    database: 'process.env.RDS_DB_NAME',
-    host: 'process.env.RDS_HOSTNAME',
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     dialect: 'postgres',
     operatorsAliases: Sequelize.Op,
