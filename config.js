72f981be156484b669022f80d5e267eebbb86874
Jay Sullivan <JaySull514@gmail.com>
2018-04-12 22:14:26 -0500

    More cleanup

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const Sequelize = require('sequelize');
 
 module.exports = {
   development: {
-    username: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
+    username: 'test',
+    password: 'test',
+    database: 'testdb',
+    host: '127.0.0.1',
+    port: 5432,
     dialect: 'postgres',
     operatorsAliases: Sequelize.Op,
   },
