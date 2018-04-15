ae79dd0ed404b775ae744d5019e1bde60340c1a8
Jay Sullivan <JaySull514@gmail.com>
2018-04-15 18:14:03 -0500

    Starting post styles

--- Components/Posts/SinglePostWrap.js
+++ Components/Posts/SinglePostWrap.js
@@ -3,4 +3,15 @@ import styled from 'react-emotion';
 export default styled('div')`
   width: 90%;
   margin-bottom: 30px;
+  a {
+    color: #72b597;
+    text-decoration: none;
+    &:visited {
+      color: #72b597;
+    }
+    &:hover {
+      color: #377c79;
+      cursor: pointer;
+    }
+  }
 `;
