70e697c07fd48e354378b6de04e4674f793eaf32
Jay Sullivan <JaySull514@gmail.com>
2018-04-15 22:11:16 -0500

    SW hack

--- utils/offline.js
+++ utils/offline.js
@@ -1,9 +1,3 @@
-/**
- * Registers our Service Worker on the site
- * Need more? check out:
- * https://github.com/GoogleChrome/sw-precache/blob/master/demo/app/js/service-worker-registration.js
- */
-
 if (
   process.env.NODE_ENV === 'production' &&
   typeof window !== 'undefined' &&
