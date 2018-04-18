5f4f490905ee487f9cee177f991b6bdc9656316e
Jay Sullivan <JaySull514@gmail.com>
2018-04-17 19:45:06 -0500

    More responsive styling

--- Components/Animation/AnimationBackdrop.js
+++ Components/Animation/AnimationBackdrop.js
@@ -3,43 +3,49 @@ import styled from 'react-emotion';
 const AnimationBackdrop = styled('div')`
   height: 300px;
   width: 300px;
+  margin-top: 20px;
   background: #efefef;
   box-shadow: 1px 1px 1px 4px #000e25;
   position: relative;
   grid-area: animation;
-  align-self: center;
+  align-self: start;
   justify-self: center;
   transition: 0.3s all ease-in;
   transform: rotateZ(53deg) rotateX(28deg) rotateY(-32deg) translateY(40px)
     translateX(40px);
 
   @media (max-width: 359px) {
+    margin-top: 12vh;
     height: 150px;
     width: 150px;
   }
   @media (min-width: 360px) {
+    margin-top: 12vh;
     height: 200px;
     width: 200px;
   }
   @media (min-width: 600px) {
-    height: 350px;
-    width: 350px;
+    margin-top: 16vh;
+    height: 250px;
+    width: 250px;
   }
-  @media (min-width: 1023px) {
-    height: 400px;
-    width: 400px;
+  @media (min-width: 768px) {
+    margin-top: 12vh;
+    height: 300px;
+    width: 300px;
   }
   @media (min-width: 1899px) {
     height: 500px;
     width: 500px;
   }
   @media (min-width: 2559px) {
-    height: 600px;
-    width: 600px;
+    margin-top: 8vh;
+    height: 700px;
+    width: 700px;
   }
   @media (min-width: 4559px) {
-    height: 600px;
-    width: 600px;
+    height: 1000px;
+    width: 1000px;
   }
 `;
 
--- Components/Grids/HeaderGrid.js
+++ Components/Grids/HeaderGrid.js
@@ -2,14 +2,49 @@ import styled from 'react-emotion';
 
 export default styled('div')`
   width: 100%;
-  max-height: 100vh;
-  min-height: 100vh;
+  height: 100vh;
+  min-height: 650px;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 8vw 50vh auto 50px;
+  grid-template-rows: minmax(100px, 8vw) minmax(200px, 40vh) minmax(115px, auto) auto;
   grid-template-areas:
     '  heading  '
     ' animation '
     'description'
     '   arrow   ';
+  @media (max-width: 359px) {
+    grid-template-rows:
+      minmax(20px, 8vw) minmax(200px, 40vh) minmax(130px, 45vh)
+      auto;
+  }
+  @media (min-width: 360px) {
+    grid-template-rows:
+      minmax(50px, 8vw) minmax(270px, 40vh) minmax(300px, 45vh)
+      auto;
+  }
+  @media (min-width: 600px) {
+    grid-template-rows:
+      minmax(70px, 8vw) minmax(350px, 40vh) minmax(200px, 45vh)
+      auto;
+  }
+  @media (min-width: 768px) {
+    grid-template-rows:
+      minmax(100px, 8vw) minmax(450px, 40vh) minmax(350px, 45vh)
+      auto;
+  }
+  @media (min-width: 1899px) {
+    grid-template-rows:
+      minmax(150px, 8vw) minmax(700px, 40vh) minmax(115px, 45vh)
+      auto;
+  }
+  @media (min-width: 2559px) {
+    grid-template-rows:
+      minmax(260px, 8vw) minmax(900px, 40vh) minmax(905px, 30vh)
+      auto;
+  }
+  @media (min-width: 4559px) {
+    grid-template-rows:
+      minmax(350px, 8vw) minmax(1000px, 40vh) minmax(1000px, 40vh)
+      auto;
+  }
 `;
